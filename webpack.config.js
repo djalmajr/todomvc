f902aa7f5cfaa0d6398b817de5c5c6552e575e00
djalmajr <djalmajr@gmail.com>
2018-11-06 21:02:26 -0300

    refactor: cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
-const CleanPlugin = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlPlugin = require("html-webpack-plugin");
-const ExternalsPlugin = require("html-webpack-externals-plugin");
-const MiniCssPlugin = require("mini-css-extract-plugin");
+const getConfig = require("wprun");
 
-const { HOT, NODE_ENV, PORT = 8080, HOST = "localhost" } = process.env;
-const DEV = NODE_ENV === "development";
+const DEV = process.env.NODE_ENV === "development";
 
-const plugins = [
-  new CleanPlugin(["dist"]),
-  new HtmlPlugin({
+module.exports = getConfig(__dirname, {
+  copyPluginOptions: [{ from: "*.ico", to: "./", context: "assets" }],
+  htmlPluginOptions: {
     title: "TodoMVC",
     description: "TodoMVC",
-    template: path.resolve(__dirname, "public/index.ejs"),
-    stylesheets: ["base.css"],
-    minify: {
-      collapseWhitespace: true,
-      removeComments: true,
-      minifyJS: true,
-      minifyCSS: true,
-    },
-  }),
-  new MiniCssPlugin({ filename: "style.css" }),
-  new CopyPlugin([
-    { from: "*.css", to: "./", context: "../public" },
-    { from: "*.ico", to: "./", context: "../public" },
-  ]),
-  new ExternalsPlugin({
-    cwpOptions: {
-      context: path.resolve(__dirname, "node_modules"),
-    },
+  },
+  externalsPluginOptions: {
     externals: [
       {
         module: "hyperhtml",
@@ -40,71 +16,5 @@ const plugins = [
         entry: DEV ? "umd.js" : "https://unpkg.com/hyperhtml@2.16.0/umd.js",
       },
     ],
-  }),
-];
-
-if (HOT) {
-  plugins.push(new webpack.HotModuleReplacementPlugin());
-}
-
-module.exports = {
-  plugins,
-  entry: "./index.js",
-  devtool: DEV ? "inline-cheap-source-map" : undefined,
-  context: path.join(__dirname, "src"),
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              emitFile: false,
-              name: "[path][name].[ext]",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          DEV ? { loader: "style-loader" } : MiniCssPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              modules: true,
-              importLoaders: true,
-              localIdentName: "[name]-[local]",
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: () => [autoprefixer],
-            },
-          },
-          { loader: "less-loader" },
-        ],
-      },
-    ],
   },
-  resolve: {
-    modules: [path.resolve(__dirname, "./src"), "node_modules"],
-  },
-  devServer: {
-    compress: true,
-    overlay: true,
-    open: true,
-    host: HOST,
-    port: PORT,
-    hot: HOT === "true",
-    publicPath: `http://localhost:${PORT}/`,
-    stats: {
-      chunks: false,
-      colors: true,
-      reasons: false,
-    },
-  },
-};
+});
