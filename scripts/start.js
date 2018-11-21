0a309aec0152165f14bab96fad73a72b6d269580
djalmajr <djalmajr@gmail.com>
2018-11-21 18:54:33 -0300

    chore: cleanup

--- scripts/start.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const cmds = `
-  cross-env NODE_ENV=development npm run copy;
-  find src -type f -name '*.css' -exec cat {} ';' > dist/style.css &&
-  live-server --open=dist .
-`;
-
-require("shelljs").exec(cmds.replace(/\n*\s+/g, " ").trim());
