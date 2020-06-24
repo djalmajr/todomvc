3e5bb2a6937f184b92abf0963f1f9dc4f7264d21
djalmajr <djalmajr@gmail.com>
2020-06-23 22:16:56 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -5,24 +5,9 @@ import {
   useState,
   virtual,
 } from "https://unpkg.com/haunted/haunted.js";
-import { Route } from "./components/awc-router.js";
-import { TodoApp } from "./components/todo-app.js";
-import { TodoFooter } from "./components/todo-footer.js";
-import { TodoHeader } from "./components/todo-header.js";
-import { TodoItem } from "./components/todo-item.js";
-import { RouterContext } from "./contexts/router.js";
-import { todoCache, TodoContext, useTodos } from "./contexts/todos.js";
-import { define } from "./helpers/dom.js";
+import { todoCache, useTodos } from "./contexts/todos.js";
 import { getRouteProps, getSlug } from "./helpers/route.js";
-
-define("todo-app", TodoApp);
-define("todo-item", TodoItem);
-define("todo-footer", TodoFooter);
-define("todo-header", TodoHeader);
-define("todo-provider", TodoContext.Provider, false);
-
-define("awc-route", Route);
-define("awc-router", RouterContext.Provider, false);
+import "./components.js";
 
 const Root = virtual(function () {
   const [todoState, todoActions] = useTodos();
